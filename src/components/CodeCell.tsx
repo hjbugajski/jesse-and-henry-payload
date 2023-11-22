3057a077c2dc6ce145bc64cbe47dea9620e64ec8
Henry Bugajski <henry.bug.dev@gmail.com>
2023-11-22 10:44:34 -0500

    fix(components/CodeCell): remove code formatting

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
 export default function CodeCell({ cellData }) {
-  const hyphenIndex = Math.floor(cellData?.length / 2);
-  const formattedData = `${cellData?.slice(0, hyphenIndex)}-${cellData?.slice(hyphenIndex)}`;
-
-  return <code className="code-cell">{formattedData?.toUpperCase()}</code>;
+  return <code className="code-cell">{cellData}</code>;
 }
