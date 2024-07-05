b2a7a13735bf961b81ae6a1ae79ac0ef3483893a
Henry Bugajski <henry.bug.dev@gmail.com>
2024-07-05 17:05:53 -0400

    fix: add default admin user

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import { config } from 'dotenv';
 import express from 'express';
 import payload from 'payload';
 
+import { Role } from './access';
+
 config();
 
 const app = express();
@@ -14,8 +16,24 @@ const start = async () => {
   await payload.init({
     secret: process.env.PAYLOAD_SECRET,
     express: app,
-    onInit: () => {
+    onInit: async ({ create, find }) => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+
+      const users = await find({
+        collection: 'users',
+        limit: 1,
+      });
+
+      if (users.docs.length === 0) {
+        await create({
+          collection: 'users',
+          data: {
+            email: process.env.PAYLOAD_ADMIN_USER,
+            password: process.env.PAYLOAD_ADMIN_PASSWORD,
+            roles: [Role.Admin],
+          },
+        });
+      }
     },
   });
 
